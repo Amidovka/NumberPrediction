be07d17900507db481248d265d638cb26afca2ff
Jan Doubek <jdoubek@cisco.com>
2014-03-24 21:55:16 +0100

    Zmena formatovani a testy

--- src/main/java/NumberPrediction.java
+++ src/main/java/NumberPrediction.java
@@ -9,12 +9,19 @@ public class NumberPrediction {
     public static void main(String[] args){
 
         SimpleRegression regrese = new SimpleRegression();
-        double[] x = new double[]{1, 2, 3, 4, 5};
-        double[] y = new double[]{1, 2, 3, 4, 5};
+//        double[] x = new double[]{1, 2, 3, 4, 5};
+        double[] x = {1,2,3,4,5};
+//        double[] y = new double[]{1, 2, 3, 4, 5};
+        double[] y = {1, 2, 3, 4, 5};
 
-        for (int i = 0; i<x.length; i++)
+        for (int i = 0; i<x.length; i++) {
             regrese.addData(x[i], y[i]);
+        }
 
         System.out.println("Predicted value is " + regrese.predict(6) + " +- " + regrese.getMeanSquareError());
+    }
+
+    public static double predictNextDouble(double[] timeSerie){
+        return 0;
     }
 }
--- src/test/java/NumberPredictionTest.java
+++ src/test/java/NumberPredictionTest.java
@@ -1,9 +1,23 @@
 import org.junit.Test;
 
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 
 /**
  * Created by amid on 23.03.14.
  */
 public class NumberPredictionTest {
+
+    @Test
+    public void testAscend(){
+        assertThat(NumberPrediction.predictNextDouble(new double[]{1, 2, 3, 4, 5}), is(equalTo(6.0)));
+    }
+
+    @Test
+    public void testJumps(){
+        assertThat(NumberPrediction.predictNextDouble(new double[]{1, 2, 3, 1, 2, 3}), is(equalTo(1.0)));
+    }
+
 }
