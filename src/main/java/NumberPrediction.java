81cc946fc94290ecacb2061be9c72fcba6d298de
Amidovka <amidovka@gmail.com>
2014-04-14 05:04:31 +0200

    multiple linear regression

--- src/main/java/NumberPrediction.java
+++ src/main/java/NumberPrediction.java
@@ -1,4 +1,9 @@
-import org.apache.commons.math3.stat.regression.SimpleRegression;
+import au.com.bytecode.opencsv.CSVReader;
+import org.apache.commons.math3.stat.regression.OLSMultipleLinearRegression;
+//import org.apache.commons.math3.stat.regression.SimpleRegression;
+
+import java.io.*;
+import java.util.List;
 
 /**
  * Created by amid on 10.03.14.
@@ -6,6 +11,56 @@ import org.apache.commons.math3.stat.regression.SimpleRegression;
 
 public class NumberPrediction {
 
+    public static double[] predictCompTimeStat(double[][] data) throws IOException{
+
+        FileReader reader = new FileReader(new File("compTimeStat.csv").getAbsolutePath());
+        CSVReader csvReader = new CSVReader(reader);
+        FileReader reader2 = new FileReader(new File("compTimeStat.csv").getAbsolutePath());
+        CSVReader csvReader2 = new CSVReader(reader2);
+
+        List<String[]> arrayData = csvReader.readAll();
+        String[] stringData = csvReader2.readNext();
+
+        double[][] xData = new double[arrayData.size()][stringData.length-1];
+        double[] yData = new double[arrayData.size()];
+
+        for (int i = 0; i < arrayData.size(); i++){
+            for (int j = 0; j < stringData.length-1; j++){
+                xData[i][j] = Double.parseDouble(stringData[j]);
+            }
+            yData[i] = Double.parseDouble(stringData[stringData.length-1]);
+            stringData = csvReader2.readNext();
+        }
+
+
+        OLSMultipleLinearRegression regression = new OLSMultipleLinearRegression();
+        regression.newSampleData(yData, xData);
+
+        double[] u = regression.estimateResiduals();
+        double[] b = regression.estimateRegressionParameters();
+
+        System.out.println("Regression parameters:");
+        for (int i = 0; i < b.length; i++){
+            System.out.println("beta"+(i)+ " = " + b[i]);
+        }
+
+        return regressionFunction(data, b, u);
+    }
+
+    // regression function Y = X*b + u
+    public static double[] regressionFunction(double[][] regressors, double[] regressionParameters, double[] residuals){
+
+        double[] y = new double[regressors.length];
+        for (int i = 0; i < regressors.length; i++){
+            for (int j = 1; j < regressionParameters.length; j++){
+                y[i] += regressors[i][j-1]* regressionParameters[j];
+            }   y[i] += regressionParameters[0] + residuals[i];
+        }
+        return y;
+    }
+}
+
+    /*
     public static double predictNextDouble(double[] timeSerie){
 
         SimpleRegression regrese = new SimpleRegression();
@@ -20,7 +75,6 @@ public class NumberPrediction {
     }
 
 
-    /*
     public static void main(String[] args){
 
         SimpleRegression regrese = new SimpleRegression();
@@ -34,5 +88,3 @@ public class NumberPrediction {
         System.out.println("Predicted value is " + regrese.predict(6) + " +- " + regrese.getMeanSquareError());
     }
     */
-
-}
--- src/test/java/NumberPredictionTest.java
+++ src/test/java/NumberPredictionTest.java
@@ -2,13 +2,14 @@ import org.junit.Test;
 
 import static org.hamcrest.core.Is.is;
 import static org.hamcrest.core.IsEqual.equalTo;
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertThat;
 
 /**
  * Created by amid on 23.03.14.
  */
 public class NumberPredictionTest {
-
+    /*
     @Test
     public void testAscend(){
         assertThat(NumberPrediction.predictNextDouble(new double[]{1, 2, 3, 4, 5}), is(equalTo(6.0)));
@@ -17,5 +18,5 @@ public class NumberPredictionTest {
     @Test
     public void testJumps(){
         assertThat(NumberPrediction.predictNextDouble(new double[]{1, 2, 3, 1, 2, 3}), is(equalTo(1.0)));
-    }
+    } */
 }
