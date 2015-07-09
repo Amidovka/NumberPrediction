f0d464011da4582e15e6cb2f0660c1fd47bbcfff
Amidovka <amidovka@gmail.com>
2015-07-10 01:16:09 +0200

    source code update

---
+++ src/main/java/ReadData.java
@@ -0,0 +1,21 @@
+import au.com.bytecode.opencsv.CSVReader;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
+
+public class ReadData {
+    /**
+     * Parsing data from the file.
+     *
+     * @return List of Strings
+     * @throws java.io.IOException
+     */
+    static List<String[]> parsedData() throws IOException {
+        FileReader reader = new FileReader(new File("Data.csv").getAbsolutePath());
+        CSVReader csvReader = new CSVReader(reader);
+
+        return csvReader.readAll();
+    }
+}
