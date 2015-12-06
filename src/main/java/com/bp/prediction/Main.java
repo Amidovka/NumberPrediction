3c0c7a9ef795209287da3ccc59174499e0d3d7e1
Dima <dmitriy.burdin@cgi.com>
2015-12-06 18:20:35 +0100

    Comment 17

--- src/main/java/com/bp/prediction/Main.java
+++ src/main/java/com/bp/prediction/Main.java
@@ -62,7 +62,7 @@ public class Main {
         chart.setRealData(yData);
         chart.setEstimatedData(regressionFunc);
         chart.draw();
-        //comment 16
+        //comment 17
         //<!--comment 4 gitignore-->
     }
 }
